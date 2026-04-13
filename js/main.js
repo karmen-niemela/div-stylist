d80b8673287dcd1f43ef081d0dc9c6a4672bdbff
Karmen Niemela <karmen@selvidata.com>
2026-04-13 17:12:40 +0300

    Disable posthog event compression

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ import posthog from 'posthog-js';
 
 posthog.init(import.meta.env.POSTHOG_API_KEY, {
   api_host: import.meta.env.POSTHOG_HOST,
+  disable_compression: true,
   autocapture: false
 });
 
